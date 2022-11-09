fe4941aeea8487bda941d0be1d8b4da4649bf8c9
sso9594 <sso9594@naver.com>
2022-11-09 10:00:31 +0900

    Initial commit

---
+++ src/test/java/com/example/teamBDC/TeamBdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamBDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
